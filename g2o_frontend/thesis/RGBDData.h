0c3900b53a700923d0cbfb4c4ae614bdd14564a0
Jacopo Serafin <jacopo.serafin@gmail.com>
2012-11-14 22:34:22 +0100

    Added RGBDData, corrected image extension in depth_registered_data, added class StampedData

---
+++ g2o_frontend/thesis/RGBDData.h
@@ -0,0 +1,58 @@
+/*
+ * RGBDData.h
+ *
+ *  Created on: Nov 14, 2012
+ *      Author: jacopo
+ */
+
+#ifndef RGBDDATA_H
+#define RGBDDATA_H
+
+#include <iosfwd>
+#include <string>
+
+#include "opencv2/highgui/highgui.hpp"
+#include "g2o/core/hyper_graph.h"
+#include "g2o/core/hyper_graph_action.h"
+#include "g2o/types/slam2d/types_slam2d.h"
+#include "g2o/types/slam3d/types_slam3d.h"
+
+class RGBDData: public g2o::HyperGraph::Data {
+public:  
+  RGBDData();
+  virtual ~RGBDData();
+  //! read the data from a stream
+  virtual bool read(std::istream& is);
+  //! write the data to a stream
+  virtual bool write(std::ostream& os) const;
+  void update();
+  void release();
+  const std::string& baseFilename() const { return _baseFilename; };
+  void  setBaseFilename(const std::string baseFilename_) { _baseFilename = baseFilename_; };
+  cv::Mat* _intensityImage;
+  cv::Mat* _depthImage;
+protected:
+  std::string _baseFilename;
+  g2o::ParameterCamera* _cameraParams;
+  long int _ts_usec;
+  long int _ts_sec;
+private:
+  int _paramIndex;
+};
+
+#ifdef G2O_HAVE_OPENGL
+
+class RGBDDataDrawAction : public g2o::DrawAction{
+public:
+  RGBDDataDrawAction() : DrawAction(typeid(RGBDData).name()) {};
+  virtual HyperGraphElementAction* operator()(g2o::HyperGraph::HyperGraphElement* element, 
+					      																g2o::HyperGraphElementAction::Parameters* params_ );
+protected:
+  virtual bool refreshPropertyPtrs(g2o::HyperGraphElementAction::Parameters* params_);
+  g2o::IntProperty* _beamsDownsampling;
+  g2o::FloatProperty* _pointSize;
+};
+
+#endif
+
+#endif
--- g2o_frontend/thesis/SensorRGBDCamera.cpp
+++ g2o_frontend/thesis/SensorRGBDCamera.cpp
@@ -15,12 +15,10 @@ SensorRGBDCamera::~SensorRGBDCamera() {
 
 }
 
-bool SensorRGBDCamera:: setParameter(g2o::Parameter* p) {
+bool SensorRGBDCamera::setParameter(g2o::Parameter* p) {
 	g2o::ParameterCamera* camParam = dynamic_cast<g2o::ParameterCamera*>(p);
 	if (camParam == 0)
 		return false;
 	_parameter = p;
-	// DA CAMBIARE (VEDERE PARAMETRI DEPTH_REGISTERED)
-	((g2o::ParameterCamera*)_parameter)->setKcam(5.75, 5.75, 3.14, 2.35);
 	return true;
 }
