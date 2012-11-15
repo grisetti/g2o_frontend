415e18eb87d41494dd88f1693a5e6ff0ffbc79a3
Hulk <fabrizio.nenci@gmail.com>
2012-11-15 01:34:38 +0100

    A first draft of OmnicamData.cpp

--- g2o_frontend/thesis/OmnicamData.cpp
+++ g2o_frontend/thesis/OmnicamData.cpp
@@ -18,8 +18,14 @@
 using namespace g2o;
 using namespace std;
 
-Omnicam::Omnicam(double timestamp_) : StampedData(timestamp_)
-{
+OmnicamData::OmnicamData(){
+	_paramIndex = -1;
+	_baseFilename = "none";
+	_cameraParams = 0;
+	_image = 0;
+}
+
+OmnicamData::OmnicamData(double timestamp_) : StampedData(timestamp_){
   _paramIndex = -1;
   _baseFilename = "none";
   _cameraParams = 0;
@@ -34,34 +40,42 @@ OmnicamData::~OmnicamData(){
 }
 
 //! read the data from a stream
-bool OmnicamData::read(std::istream& is) 
-{
-  int _paramIndex;
-  is >> _paramIndex >> _baseFilename;
-  is >> _ts_sec >> _ts_usec;
-  _intensityImage = 0;
-  _depthImage = 0;
-  update();
-  return true;
+bool OmnicamData::read(std::istream& is) {
+	int r;
+	is >> r;	
+	return false;
 }
 
 //! write the data to a stream
-bool OmnicamData::write(std::ostream& os) const 
-{
-  if (_cameraParams)
-    os << _cameraParams->id();
-  else
-    os << -1;
-  os << " " <<  _baseFilename << " ";
-  os << _ts_sec << " " << _ts_usec;
-  return true;
+bool OmnicamData::write(std::ostream& os) const {
+	int firstId = _nextId;
+	// write the robot pose vertex
+	os << "VERTEX_SE2 " << firstId << " " << _pose[0] << " " << _pose[1] << " " << _pose[2] << std::endl;
+	
+	// for every landmark seen from here
+	for(unsigned int i=0; i<_observations.size(); i++){
+		// write the landmark position
+		os << "VERTEX_XY " << firstId+i+1;
+		_observed_landmarks[i].write(os);
+		os << std::endl;
+		
+		// write the constraints
+		os << "EDGE_BEARING_SE2_XY " << firstId << " " << firstId+i+1 << " " << _observations[i] << " " << 200;
+	}
+	return false;
 }
 
 // set the odometry
-void setPose(double x, duble y, double theta){
+void OmnicamData::setPose(double x, double y, double theta){
 	_pose[0] = x;
 	_pose[1] = y;
 	_pose[2] = theta;
+}
+
+// add a constraint
+void OmnicamData::addObservation(double bearing, g2o::VertexPointXY landmark){
+	_observations.push_back(bearing);
+	_observed_landmarks.push_back(landmark);
 }
 
 G2O_REGISTER_TYPE(OMNICAM_DATA, OmnicamData);
