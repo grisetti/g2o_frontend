f93b22e050a61b3b5c9016754e50e614de91cb71
rococo-rpcX2 <erratic@rococo-rpcX2.(none)>
2013-04-08 17:41:13 +0200

    Little change for the new eigen version with the direct solver.

--- cmake_modules/FindEigen3.cmake
+++ cmake_modules/FindEigen3.cmake
@@ -63,7 +63,7 @@ else (EIGEN3_INCLUDE_DIR)
 
   find_path(EIGEN3_INCLUDE_DIR NAMES signature_of_eigen3_matrix_library
       PATHS
-      /ust/local/include/eigen3
+      /usr/local/include/eigen3     
       ${CMAKE_INSTALL_PREFIX}/include
       ${KDE4_INCLUDE_DIR}
       PATH_SUFFIXES eigen3 eigen
