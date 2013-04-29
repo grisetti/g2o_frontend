75e47b57d1541f50ac29de84599770f7150ec411
rococo-rpcX2 <erratic@rococo-rpcX2.(none)>
2013-04-29 17:32:42 +0200

    normal computation flow rewritten need only to change aligner in order to take two scenes.

--- g2o_frontend/pwn/homogeneouspoint3fscene.h
+++ g2o_frontend/pwn/homogeneouspoint3fscene.h
@@ -10,23 +10,28 @@ class HomogeneousPoint3fScene {
  public:
   HomogeneousPoint3fScene() {}
   
+  DepthImage& depthImage() { return _depthImage; }
+  Eigen::MatrixXi& indexImage() { return _indexImage; }
   HomogeneousPoint3fVector& points() { return _points; }
   HomogeneousNormal3fVector& normals() { return _normals; }
   HomogeneousPoint3fStatsVector& stats() { return _stats; }
   HomogeneousPoint3fOmegaVector& pointOmegas() { return _pointOmegas; }
   HomogeneousPoint3fOmegaVector& normalOmegas() { return _normalOmegas; }
-  CorrespondenceVector& correspondences() { return _correspondences; }
-  int& numCorrespondences() { return _numCorrespondences; }
+  Eigen::Isometry3f cameraPose() { return _cameraPose; }
 
-  void setPoints(HomogeneousPoint3fVector points_) { _points = points_; }
-  void setNormals(HomogeneousNormal3fVector normals_) { _normals = normals_; }
-  void setStats(HomogeneousPoint3fStatsVector stats_) { _stats = stats_; }
-  void setPointOmegas(HomogeneousPoint3fOmegaVector pointOmegas_) { _pointOmegas = pointOmegas_; }
-  void setNormalOmegas(HomogeneousPoint3fOmegaVector normalOmegas_) { _normalOmegas = normalOmegas_; }
-  void setCorrespondences(CorrespondenceVector correspondences_) { _correspondences = correspondences_; }
-  void setNumCorrespondences(int numCorrespondences_) { _numCorrespondences = numCorrespondences_; }
+  void setDepthImage(DepthImage &depthImage_) { _depthImage = depthImage_; }
+  void setIndexImage(Eigen::MatrixXi &indexImage_) { _indexImage = indexImage_; }
+  void setPoints(HomogeneousPoint3fVector &points_) { _points = points_; }
+  void setNormals(HomogeneousNormal3fVector &normals_) { _normals = normals_; }
+  void setStats(HomogeneousPoint3fStatsVector &stats_) { _stats = stats_; }
+  void setPointOmegas(HomogeneousPoint3fOmegaVector &pointOmegas_) { _pointOmegas = pointOmegas_; }
+  void setNormalOmegas(HomogeneousPoint3fOmegaVector &normalOmegas_) { _normalOmegas = normalOmegas_; }
+  void setCameraPose(Eigen::Isometry3f cameraPose_) { _cameraPose = cameraPose_; }
 
  protected:
+  DepthImage _depthImage;
+  Eigen::MatrixXi _indexImage;
+
   HomogeneousPoint3fVector _points;
   HomogeneousNormal3fVector _normals;
   
@@ -34,9 +39,8 @@ class HomogeneousPoint3fScene {
   
   HomogeneousPoint3fOmegaVector _pointOmegas;
   HomogeneousPoint3fOmegaVector _normalOmegas;
-  
-  CorrespondenceVector _correspondences;
-  int _numCorrespondences;
+
+  Eigen::Isometry3f _cameraPose;
 };
 
 std::vector<HomogeneousPoint3fScene> HomogeneousPoint3fSceneVector;
