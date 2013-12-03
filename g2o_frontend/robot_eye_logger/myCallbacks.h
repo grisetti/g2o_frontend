7cc74d05b94ca20153cb422c0bdb04bec37a0f89
Martina <mdeturres@gmail.com>
2013-12-03 19:38:51 +0100

    RobotEye logger v2.0, saving files for octave uses and a pcd format file

--- g2o_frontend/robot_eye_logger/myCallbacks.h
+++ g2o_frontend/robot_eye_logger/myCallbacks.h
@@ -2,32 +2,37 @@
 #define _MY_CALLBACKS
 
 #include <iostream>
-#include "RobotEye.h"
-#include "RE05Driver.h"
+#include "Eigen/Core"
+#include "utilities.h"
 
-//#include "mutex.h"
+#include "mutex.h"
 
-std::vector<ocular::ocular_rbe_obs_t> measurements;
-//Mutex* mutex_meas;
+std::vector<ocular::ocular_rbe_obs_t> _measurements; // [az, el, range, intensity]
+std::vector<Eigen::Vector4d> _xyz_meas; // [x, y, z, intensity]
+Mutex _mutex_meas;
 
 
 class AngleCB : public ocular::RobotEyeApertureAnglesCallbackClass{
 
   virtual void ApertureAnglesCallback(double az, double el, unsigned int timestamp){
-        std::cerr << "angle callback triggered: az " << az << " ,el " << el << ", timestamp" << timestamp << std::endl;
+//        std::cerr << "angle callback triggered: az " << az << " ,el " << el << ", timestamp" << timestamp << std::endl;
+        std::cerr << ".";
   }
 };
 
 class LaserCB : public ocular::RobotEyeLaserDataCallbackClass{
 
   virtual void LaserDataCallback(std::vector<ocular::ocular_rbe_obs_t> observations){
-    std::cerr << "laser callback triggered" << std::endl;
+//    std::cerr << "laser callback triggered" << std::endl;
+    std::cerr << ".";
     // in case of parallel callbacks that would modify the measurements vector, not needed
-//    mutex_meas->lock();
-    for(int i = 0; i<= observations.size(); i++){
-        measurements.push_back(observations[i]);
+    _mutex_meas.lock();
+    for(unsigned int i = 0; i <= observations.size(); i++){
+        Eigen::Vector4d xyz_int = polar2euclidean(observations[i]);
+        _xyz_meas.push_back(xyz_int);
+        _measurements.push_back(observations[i]); //to print the data
     }
-//    mutex_meas->unlock();
+    _mutex_meas.unlock();
   }
 };
 
