f4d9f8c9f5c38fa6f09c482fbb5d269bca779dd4
tambo83 <bonanni@dis.uniroma1.it>
2014-01-14 14:24:33 +0100

    new stuff for voronoi

--- g2o_frontend/voronoi/voronoi_edge.cpp
+++ g2o_frontend/voronoi/voronoi_edge.cpp
@@ -8,7 +8,6 @@ using namespace Eigen;
 
 VoronoiEdge::VoronoiEdge()
 {
-    _from = 0;
     _to = 0;
 }
 
@@ -22,8 +21,37 @@ VoronoiEdge::VoronoiEdge(VoronoiVertex* from_, VoronoiVertex* to_)
 
 bool EdgeComparator::operator()(const VoronoiEdge& lhs, const VoronoiEdge& rhs) const
 {
-    if((lhs.from()->position().x() < rhs.from()->position().x())
-            || ((lhs.from()->position().x() == rhs.from()->position().x()) && (lhs.from()->position().y() < rhs.from()->position().y())))
+    const VoronoiVertex* lhs_from = lhs.from();
+    const int lhs_from_x = lhs_from->position().x();
+    const int lhs_from_y = lhs_from->position().y();
+
+    const VoronoiVertex* lhs_to = lhs.to();
+    const int lhs_to_x = lhs_to->position().x();
+    const int lhs_to_y = lhs_to->position().y();
+
+
+    const VoronoiVertex* rhs_from = rhs.from();
+    const int rhs_from_x = rhs_from->position().x();
+    const int rhs_from_y = rhs_from->position().y();
+
+    const VoronoiVertex* rhs_to = rhs.to();
+    const int rhs_to_x = rhs_to->position().x();
+    const int rhs_to_y = rhs_to->position().y();
+
+
+    if((lhs_from->position() == rhs_from->position()) && (lhs_to_x == rhs_to_x) && (lhs_to_y < rhs_to_y))
+    {
+        return true;
+    }
+    else if((lhs_from->position() == rhs_from->position()) && (lhs_to_x < rhs_to_x))
+    {
+        return true;
+    }
+    else if((lhs_from_x == rhs_from_x) && (lhs_from_y < rhs_from_y))
+    {
+        return true;
+    }
+    else if(lhs_from_x < rhs_from_x)
     {
         return true;
     }
