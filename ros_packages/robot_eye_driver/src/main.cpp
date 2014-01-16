43c62050da482f80171064c962c8aaa3f84ec1f6
Martina <mdeturres@gmail.com>
2014-01-16 20:15:17 +0100

    roboteyenode publisher safe commit 2.0

--- ros_packages/robot_eye_driver/src/main.cpp
+++ ros_packages/robot_eye_driver/src/main.cpp
@@ -2,65 +2,67 @@
 
 using namespace std;
 
+//global parameters
+double az_rate;
+double n_lines;
+double laser_freq;
+double averaging;
+bool intensity;
+
 void init_parameters(int argc, char ** argv){
 
     // initialize parameters
     std::string outfilename;
-    int _seconds;
-    double _az_rate;
-    double _N_lines;
-    double _laser_freq;
-    double _averaging;
-    bool _intensity;
+//    int seconds;
     g2o::CommandArgs arg;
-    arg.param("t",_seconds, 15, "choose for how many seconds you want to exec this routine");
-    arg.param("az_rate", _az_rate, 10, "set the number of rounds per second [max:15(5400 degrees/sec)]");
-    arg.param("nlines", _N_lines, 100, "set the number of horizontal lines (vertical resolution) [min:_az_rate]");
-    arg.param("avg", _averaging, 1, "set the 'averaging' value range:[1,5]");
-    arg.param("lfreq", _laser_freq, 10000, "set the measurement frequency [range:[1, 30000]]");
-    arg.param("intensity", _intensity, 0, "enable the streaming of intensity values [can be 'on' only if lfreq <= 10000] default off");
+//    arg.param("t",seconds, 15, "choose for how many seconds you want to exec this routine");
+    arg.param("az_rate", az_rate, 10, "set the number of rounds per second [max:15(5400 degrees/sec)]");
+    arg.param("nlines", n_lines, 100, "set the number of horizontal lines (vertical resolution) [min:_az_rate]");
+    arg.param("avg", averaging, 1, "set the 'averaging' value range:[1,5]");
+    arg.param("lfreq", laser_freq, 10000, "set the measurement frequency [range:[1, 30000]]");
+    arg.param("intensity", intensity, 0, "enable the streaming of intensity values [can be 'on' only if lfreq <= 10000] default off");
     arg.param("o", outfilename, "Eyebot_pcd.pcd", "output filename of a pcd format file");
 
     arg.parseArgs(argc, argv);
 
     // check parameters consistency
-    if(_averaging > 5){
+    if(averaging > 5){
         cout << "-avg must be an integer value from 1 to 5" << endl << endl;
         exit(1);
     }
 
-    if(_laser_freq > 30000 || _laser_freq == 0){
+    if(laser_freq > 30000 || laser_freq == 0){
         cout << "-lfreq must be an integer value in [1, 30000]" << endl << endl;
         exit(1);
     }
 
-    if(_az_rate > 15 || _az_rate == 0){
+    if(az_rate > 15 || az_rate == 0){
         cout << "-az_rate must be a value in (0, 15]" << endl << endl;
         exit(1);
     }
 
-    if(_N_lines < _az_rate){
+    if(n_lines < az_rate){
         cout << "-nlines must be at least equal to az_rate" << endl << endl;
         exit(1);
     }
 
-    if(_intensity && _laser_freq > 10000){
+    if(intensity && laser_freq > 10000){
         cout << "-intensity can't be specified for lfreq values higher than 10000" << endl << endl;
         exit(1);
     }
 
-    if(_seconds < 1){
-        cout << "-t must be an integer positive non zero value" << endl << endl;
-        exit(1);
-    }
+//    if(seconds < 1){
+//        cout << "-t must be an integer positive non zero value" << endl << endl;
+//        exit(1);
+//    }
 
     // print parameters
-    cout << "LASER FREQUENCY:\t" << _laser_freq << endl;
-    cout << "AVERAGING:\t" << _averaging << endl;
-    cout << "GET INTENSITY:\t" << (_intensity? "YES" : "NO") << endl;
-    cout << "AZIMUTH RATE:\t" << _az_rate << endl;
-    cout << "N_LINES:\t" << _N_lines << endl;
-    cout << "RUNNING FOR:\t" << _seconds << " SECONDS" << endl;
+    cout << "LASER FREQUENCY:\t" << laser_freq << endl;
+    cout << "AVERAGING:\t" << averaging << endl;
+    cout << "GET INTENSITY:\t" << (intensity? "YES" : "NO") << endl;
+    cout << "AZIMUTH RATE:\t" << az_rate << endl;
+    cout << "N_LINES:\t" << n_lines << endl;
+//    cout << "RUNNING FOR:\t" << seconds << " SECONDS" << endl;
 
 }
 
@@ -71,7 +73,8 @@ int main(int argc, char **argv)
     init_parameters(argc,argv);
 
     ros::init(argc, argv, "roboteye_node");
-//    roboteye_node re;
+    ros::Time::init();
+    roboteye_node re(az_rate, n_lines, laser_freq, averaging, intensity);
     ros::Rate r(10);
     ros::spin();
 
--- ros_packages/robot_eye_driver/src/roboteye_node.h
+++ ros_packages/robot_eye_driver/src/roboteye_node.h
@@ -12,143 +12,55 @@
 #include <boost/thread/thread.hpp>
 #include <g2o/stuff/command_args.h>
 
-#include "RE05Driver.h"
 #include "roboteyestruct.h"
-//#include "mutex.h"
+#include "robot_eye_callback.h"
 
 #include <ros/ros.h>
 #include <robot_eye_driver/RobotEyeScan.h>
 
-
-////global variables
-//roboteye::PolarMeasurements _meas_all; // [az, el, range, intensity]
-//roboteye::PolarMeasurements _meas_current; // [az, el, range, intensity]
-//roboteye::PolarVector _meas_all_vector;
-//roboteye::EuclideanMeasurements _xyz_meas_all; // [x, y, z, intensity]
-//roboteye::EuclideanMeasurements _xyz_meas_current; // [x, y, z, intensity]
-//roboteye::EuclideanVector _xyz_meas_all_vector;
-
-typedef std::vector<ocular::ocular_rbe_obs_t>  PolarMeasurements;
-typedef std::vector<PolarMeasurements> PolarVector;
-
-typedef std::vector<Eigen::Vector4d>  EuclideanMeasurements;
-typedef std::vector<EuclideanMeasurements> EuclideanVector;
-
-inline double deg2rad(double deg){
-  return deg*M_PI/180;
-}
-
-class AngleCB : public ocular::RobotEyeApertureAnglesCallbackClass{
-
-    virtual void ApertureAnglesCallback(double az, double el, unsigned int timestamp){
-        //        std::cerr << "angle callback triggered: az " << az << " ,el " << el << ", timestamp" << timestamp << std::endl;
-        std::cerr << ".";
-    }
-};
-
-class LaserCB : public ocular::RobotEyeLaserDataCallbackClass{
-
-protected:
-    PolarMeasurements _meas_all; // [az, el, range, intensity]
-    PolarMeasurements _meas_current; // [az, el, range, intensity]
-    PolarVector _meas_all_vector;
-    EuclideanMeasurements _xyz_meas_all; // [x, y, z, intensity]
-    EuclideanMeasurements _xyz_meas_current; // [x, y, z, intensity]
-    EuclideanVector _xyz_meas_all_vector;
-    //    Mutex _mutex_meas;
-
-public:
-    virtual void LaserDataCallback(std::vector<ocular::ocular_rbe_obs_t> observations){
-        //    std::cerr << "laser callback triggered" << std::endl;
-        std::cerr << observations.size() << " ";
-
-        // in case of parallel callbacks that would modify the measurements vector, not needed
-//            _mutex_meas.lock();
-
-        _meas_current.clear();
-        _xyz_meas_current.clear();
-
-        EuclideanMeasurements xyz_meas_tmp;
-
-        for(unsigned int i = 0; i <= observations.size(); i++){
-            Eigen::Vector4d xyzi = polar2euclidean(observations[i]);
-            xyz_meas_tmp.push_back(xyzi);
-
-            _meas_current.push_back(observations[i]);
-            _xyz_meas_current.push_back(xyzi);
-
-            //adding the current measurement in the global structures to cout/print the whole data at the end
-            _meas_all.push_back(observations[i]);
-            _xyz_meas_all.push_back(xyzi);
-
-        }
-        _meas_all_vector.push_back(observations);
-        _xyz_meas_all_vector.push_back(xyz_meas_tmp);
-
-//            _mutex_meas.unlock();
-    }
-    Eigen::Vector4d polar2euclidean(ocular::ocular_rbe_obs_t obs){
-
-      // az : rotation around z-axis (on this sensor it's considered positive in clockwise way)
-      // el : rotation around y-axis (a negative rotation means to look down (positive in counterclockwise way))
-
-      double az = deg2rad(-obs.azimuth);
-      double el = deg2rad(-obs.elevation);
-      double t = obs.range;
-      unsigned short int intensity = obs.intensity;
-
-      double c_az = cos(az);
-      double s_az = sin(az);
-
-      double c_el = cos(el);
-      double s_el = sin(el);
-
-
-      Eigen::Matrix4d rot_az, rot_el;
-      Eigen::Vector4d trasl(t, 0, 0, 1);
-      rot_az <<
-        c_az, -s_az, 0, 0,
-        s_az,  c_az, 0, 0,
-        0   ,   0  , 1, 0,
-        0   ,   0  , 0, 1;
-
-      rot_el <<
-        c_el ,  0 , s_el, 0,
-        0    ,  0 ,  0  , 0,
-        -s_el,  0 , c_el, 0,
-        0    ,  0 ,  0  , 1;
-
-      //computing the xyz_intensity coordinate in the Euclidean Space
-      Eigen::Vector4d xyz_intensity = rot_az * rot_el * trasl;
-      xyz_intensity[3] = intensity;
-
-      return xyz_intensity;
-    }
-
-};
-
-class NotificationCB : public ocular::RobotEyeNotificationCallbackClass{
-
-    virtual void NotificationCallback(ocular::ocular_error_t ErrCode){
-        std::cerr << "notification callback triggered" << std::endl;
-        if(ErrCode != 0)
-            std::cerr << "with errorcode = " << ErrCode << std::endl;
-    }
-};
-
 class roboteye_node
 {
 public:
-    roboteye_node();
+    roboteye_node(double az_rate, double n_lines, double laser_freq, double averaging, bool intensity);
+    ~roboteye_node();
+
+    roboteye::RobotEyeConfig getConfig() {return _scan_config; }
+    void setConfig(roboteye::RobotEyeConfig scan_config) {_scan_config = scan_config; }
+    void stopAndPrint();
+    void printAndWriteLaserData();
+    void roboteyeRunning();
+
     // config parameters
     std::string _sensor_IP;
+//    int _seconds;
+    double _az_rate;
+    double _N_lines;
+    double _laser_freq;
+    double _averaging;
+    bool _intensity;
 
 
 protected:
-    AngleCB _angle_callback;
+//    AngleCB _angle_callback;
     LaserCB _laser_callback;
-    NotificationCB _notif_callback;
+//    NotificationCB _notif_callback;
     ocular::RE05Driver* laserone;
+    unsigned int _num_readings;
+    double _desired_freq;
+    double _lastStamp;
+
+    /*Ros specific stuff*/
+    ros::NodeHandle _handle;
+    ros::Publisher _scan_pub;
+    robot_eye_driver::RobotEyeScan _scan_msg;
+    roboteye::RobotEyeConfig _scan_config;
+    //    std::vector<roboteye::RobotEyeScan> _scan_vector;
+    roboteye::RobotEyeScan _scan;
+
+    /*Boost specific stuff*/
+    boost::thread _thrd;
+//    boost::mutex mtx;
+
 };
 
 
