5657397c05947f18b004aef2b6d1322fa2860750
Giorgio Grisetti <grisetti@informatik.uni-freiburg.de>
2014-03-11 16:43:51 +0100

    nice mappe

--- g2o_frontend/g2o2ros/frequency_map.cpp
+++ g2o_frontend/g2o2ros/frequency_map.cpp
@@ -31,6 +31,7 @@ void FrequencyMap::integrateScan(const g2o::RawLaser *laser, const g2o::SE2 &rob
 
   g2o::SE2 laserCenter = robotPose * laser->laserParams().laserPose;
   Eigen::Vector2f rp = Eigen::Vector2f((float) laserCenter.translation().x(), (float) laserCenter.translation().y());
+
   Eigen::Vector2i start = world2map(rp);
   for(size_t i = 0; i < laser->ranges().size(); i++) {
     float r = (float) laser->ranges()[i];
@@ -42,23 +43,23 @@ void FrequencyMap::integrateScan(const g2o::RawLaser *laser, const g2o::SE2 &rob
     }
     static GridLineTraversalLine line;
     Eigen::Vector2d bp(r * cosf(laser->laserParams().firstBeamAngle + i * laser->laserParams().angularStep), 
-		       r * sinf(laser->laserParams().firstBeamAngle + i * laser->laserParams().angularStep));
+  		       r * sinf(laser->laserParams().firstBeamAngle + i * laser->laserParams().angularStep));
     bp = laserCenter * bp;    
     Eigen::Vector2i end = world2map(bp.cast<float>());
       
     GridLineTraversal::gridLine(start, end, &line);
     for(int i = 0; i < line.numPoints; i++) {
-      if(isInside(line.points[i])) { this->coeffRef(line.points[i].x(), line.points[i].y()).incrementMisses(gain); }
+      if(isInside(line.points[i])) { this->coeffRef(line.points[i].x(), line.points[i].y()).incrementMisses(1); }
     }
     if(!isInside(end)) { continue; }
     if(!cropped) { 
       for(int c = -squareSize; c <= squareSize; c++) {
-	for(int r = -squareSize; r <= squareSize; r++) {
-	  Eigen::Vector2i subCell(end.x() + r, end.y() + c);
-	  if(isInside(subCell)) {
-	    this->coeffRef(subCell.x(), subCell.y()).incrementHits(gain); 
-	  }
-	}
+  	for(int r = -squareSize; r <= squareSize; r++) {
+  	  Eigen::Vector2i subCell(end.x() + r, end.y() + c);
+  	  if(isInside(subCell)) {
+  	    this->coeffRef(subCell.x(), subCell.y()).incrementHits(gain); 
+  	  }
+  	}
       }
     }
   }
--- g2o_frontend/g2o2ros/g2o2ros.cpp
+++ g2o_frontend/g2o2ros/g2o2ros.cpp
@@ -26,7 +26,7 @@ int main(int argc, char **argv) {
    *                          Input handling                              *
    ************************************************************************/
   int threshold, rows, cols, gain, square_size;
-  float resolution, max_range, usable_range;
+  float resolution, max_range, usable_range, angle;
   string g2oFilename, mapFilename;
   g2o::CommandArgs arg;
   arg.param("resolution", resolution, 0.05f, "resolution of the map (how much is in meters a pixel)");
@@ -37,9 +37,12 @@ int main(int argc, char **argv) {
   arg.param("usable_range", usable_range, -1.0f, "usable laser range for map building");
   arg.param("gain", gain, 1, "gain to impose to the pixels of the map");
   arg.param("square_size", square_size, 1, "square size of the region where increment the hits");
+  arg.param("angle", angle, 0, "rotate the map of x degrees");
   arg.paramLeftOver("input_graph.g2o", g2oFilename, "", "input g2o graph to use to build the map", false);
   arg.paramLeftOver("output_map", mapFilename, "", "output filename where to save the map (without extension)", false);  
   arg.parseArgs(argc, argv);
+
+  angle = angle*M_PI/180.0;
 
   /************************************************************************
    *                          Loading Graph                               *
@@ -70,11 +73,20 @@ int main(int argc, char **argv) {
   Eigen::Matrix2d boundingBox = Eigen::Matrix2d::Zero();
   std::vector<RobotLaser*> robotLasers;
   std::vector<SE2> robotPoses;
+  double xmin=std::numeric_limits<double>::max();
+  double xmax=std::numeric_limits<double>::min();
+  double ymin=std::numeric_limits<double>::max();
+  double ymax=std::numeric_limits<double>::min();
+
+  SE2 baseTransform(0,0,angle);
+
   for(size_t i = 0; i < vertexIds.size(); ++i) {
     OptimizableGraph::Vertex *_v = graph->vertex(vertexIds[i]);
     VertexSE2 *v = dynamic_cast<VertexSE2*>(_v);
     if(!v) { continue; }
+    v->setEstimate(baseTransform*v->estimate());
     OptimizableGraph::Data *d = v->userData();
+
     while(d) {
       RobotLaser *robotLaser = dynamic_cast<RobotLaser*>(d);
       if(!robotLaser) {
@@ -83,15 +95,23 @@ int main(int argc, char **argv) {
       }      
       robotLasers.push_back(robotLaser);
       robotPoses.push_back(v->estimate());
-      Eigen::Vector2f translation = Eigen::Vector2f((float) v->estimate().translation().x(),
-						    (float) v->estimate().translation().y());
-      if(translation.x() < boundingBox(0, 0)) { boundingBox(0, 0) = translation.x(); }
-      if(translation.x() >= boundingBox(0, 1)) { boundingBox(0, 1) = translation.x(); }
-      if(translation.y() < boundingBox(1, 0)) { boundingBox(1, 0) = translation.y(); }
-      if(translation.y() >= boundingBox(1, 1)) { boundingBox(1, 1) = translation.y(); }
+      double x = v->estimate().translation().x();
+      double y = v->estimate().translation().y();
+      
+      xmax = xmax > x+usable_range ? xmax : x+usable_range;
+      ymax = ymax > y+usable_range ? ymax : y+usable_range;
+      xmin = xmin < x-usable_range ? xmin : x-usable_range;
+      ymin = ymin < y-usable_range ? ymin : y-usable_range;
+ 
       d = d->next();
     }
   }
+
+  boundingBox(0,0)=xmin;
+  boundingBox(0,1)=xmax;
+  boundingBox(1,0)=ymin;
+  boundingBox(1,1)=ymax;
+
   std::cout << "Found " << robotLasers.size() << " laser scans"<< std::endl;
   std::cout << "Bounding box: " << std::endl << boundingBox << std::endl; 
   if(robotLasers.size() == 0)  {
@@ -103,14 +123,11 @@ int main(int argc, char **argv) {
    *                          Compute the map                             *
    ************************************************************************/
   // Create the map
-  float maxRange;
-  if(max_range < 0.0f) { maxRange = robotLasers[0]->laserParams().maxRange; }
-  else { maxRange = max_range; }
   Eigen::Vector2i size;
   if(rows != 0 && cols != 0) { size = Eigen::Vector2i(rows, cols); }
   else {
-    size = Eigen::Vector2i((boundingBox(0, 1) - boundingBox(0, 0) + maxRange / 2.0f) / resolution,
-			   (boundingBox(1, 1) - boundingBox(1, 0) + maxRange / 2.0f)/ resolution);
+    size = Eigen::Vector2i((boundingBox(0, 1) - boundingBox(0, 0))/ resolution,
+			   (boundingBox(1, 1) - boundingBox(1, 0))/ resolution);
     } 
   std::cout << "Map size: " << size.transpose() << std::endl;
   if(size.x() == 0 || size.y() == 0) {
@@ -118,15 +135,29 @@ int main(int argc, char **argv) {
     return 0;
   }
 
-  Eigen::Vector2f offset(-size.x() * resolution / 2.0f, -size.y() * resolution / 2.0f);
+  
+
+  //Eigen::Vector2f offset(-size.x() * resolution / 2.0f, -size.y() * resolution / 2.0f);
+  Eigen::Vector2f offset(boundingBox(0, 0),boundingBox(1, 0));
   FrequencyMapCell unknownCell;
   FrequencyMap map = FrequencyMap(resolution, offset, size, unknownCell);
 
-  // Fill the map with scans
-  for(size_t i = 0; i < robotLasers.size(); i++) {
-    RobotLaser *robotLaser = robotLasers[i];
-    SE2 robotPose = robotPoses[i];
-    map.integrateScan(robotLaser, robotPose, max_range, usable_range, gain, square_size);
+  for(size_t i = 0; i < vertexIds.size(); ++i) {
+    OptimizableGraph::Vertex *_v = graph->vertex(vertexIds[i]);
+    VertexSE2 *v = dynamic_cast<VertexSE2*>(_v);
+    if(!v) { continue; }
+    OptimizableGraph::Data *d = v->userData();
+    SE2 robotPose = v->estimate();
+    
+    while(d) {
+      RobotLaser *robotLaser = dynamic_cast<RobotLaser*>(d);
+      if(!robotLaser) {
+	d = d->next();
+	continue;
+      }      
+      map.integrateScan(robotLaser, robotPose, max_range, usable_range, gain, square_size);
+      d = d->next();
+    }
   }
 
   /************************************************************************
@@ -138,16 +169,20 @@ int main(int argc, char **argv) {
     for(int r = 0; r < map.rows(); r++) {
       if(map(r, c).misses() == 0 && map(r, c).hits() == 0) {
 	mapImage.at<unsigned char>(r, c) = 127;
-      } 
-      else if(map(r, c).hits() > threshold) {
-	mapImage.at<unsigned char>(r, c) = 255;
+      } else {
+	float fraction = map(r, c).hits()/(map(r, c).hits()+map(r, c).misses());
+	float val = 255*(1-fraction);
+	mapImage.at<unsigned char>(r, c) = (unsigned char)val;
       }
-      else {
-	mapImage.at<unsigned char>(r, c) = 0;
-      }
+      // else if(map(r, c).hits() > threshold) {
+      // 	mapImage.at<unsigned char>(r, c) = 255;
+      // }
+      // else {
+      // 	mapImage.at<unsigned char>(r, c) = 0;
+      // }
     }
   }
-  cv::imwrite(mapFilename + ".png", cv::Scalar(255) - mapImage);
+  cv::imwrite(mapFilename + ".png", mapImage);
 
   /************************************************************************
    *                          Write yaml file                             *
